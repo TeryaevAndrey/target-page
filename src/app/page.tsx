b6dfab18c159b49db2e68345b1245414bfc08ba0
Teryaev Andrey <teryaev.2004@mail.ru>
2023-06-21 23:13:37 +0300

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Header } from "@/components";
 
 export default function Home() {
   return (
-    <div></div>
-  )
+    <div>
+      <Header />
+    </div>
+  );
 }
